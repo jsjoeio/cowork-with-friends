7868497fe1e4f3ee011a2cb39928a94bb755f48e
Joe Previte <jjprevite@gmail.com>
2024-02-04 20:30:21 +0000

    feat: add basic banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+export function Banner() {
+  return (
+    <div className="bg-pink py-1">
+      <p className="animate-marquee flex items-center leading-10 font-semibold uppercase text-black font-black text-2xl whitespace-nowrap tracking-tighter">
+        {" "}
+        The best thing you can do for your loneliness • The best thing you can
+        do for your loneliness • The best thing you can do for your loneliness •
+        The best thing you can do for your loneliness • The best thing you can
+        do for your loneliness • The best thing you can do for your loneliness •
+        The best thing you can do for your loneliness • The best thing you can
+        do for your loneliness • The best thing you can do for your loneliness •
+        The best thing you can do for your loneliness •{" "}
+      </p>
+    </div>
+  )
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,22 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFCF0",
+        black: "#100F0F",
+        pink: "#EF9CCB",
+      },
+      animation: {
+        marquee: "marquee 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
