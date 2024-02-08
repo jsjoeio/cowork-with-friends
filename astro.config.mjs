aff7f3018482648487a6900f8aa99f06b7e72878
Joe Previte <jjprevite@gmail.com>
2024-02-08 15:56:36 +0000

    fixup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.coworkwithfriends.com",
-  integrations: [tailwind(), react(), sitemap()]
-});
+  site: "https://coworkwithfriends.com",
+  integrations: [tailwind(), react(), sitemap()],
+})
