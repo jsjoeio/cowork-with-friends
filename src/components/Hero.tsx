aadc07c72226ebb3ffe7b716cf9c3f51c11f0462
Joe Previte <jjprevite@gmail.com>
2024-02-08 04:47:30 +0000

    feat: ads sign up form

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,9 @@ function Button() {
   return (
     <div>
       <div className="inline-block relative z-[2] w-full xl:w-auto xl:w-[300px]">
-        <button
+        <a
+          href="https://forms.reform.app/WR4x5f/cwf-signup-form/41rn5c"
+          target="_blank"
           className="border-2 border-light-green relative z-2 w-full text-[18px] z-1 font-bold rounded-full flex justify-center text-center whitespace-nowrap"
           style={{ padding: "18px 40px" }}
         >
@@ -10,7 +12,7 @@ function Button() {
             {/**/}
             <span className="text-white">Let's do it</span>
           </span>
-        </button>
+        </a>
         <span className="block absolute z-[-2] w-full h-full bottom-[-4px] right-[-4px] rounded-full bg-dark-green" />
       </div>
     </div>
