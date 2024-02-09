4d5acba8b64a6a62bb371b28da3ef2715dea344e
Joe Previte <jjprevite@gmail.com>
2024-02-09 03:42:05 +0000

    feat: add partytown and beam analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import react from "@astrojs/react"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from "@astrojs/sitemap"
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://coworkwithfriends.com",
-  integrations: [tailwind(), react(), sitemap()],
-})
+  integrations: [tailwind(), react(), sitemap(), partytown()]
+});
