a766d56b77b8cc2a96ed72d5ead5f17e9ed0b610
Joe Previte <jjprevite@gmail.com>
2024-03-01 15:28:42 +0000

    feat: add groups collection

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { z, defineCollection } from "astro:content"
+
+const groupsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    name: z.string(), // The name of the group i.e. "Cowork With Friends Scottsdale"
+    description: z.string(), // A group of tech professionals who cowork in Scottsdale, AZ
+    image: z.object({
+      // Cover image for the group
+      url: z.string().url(),
+      alt: z.string(),
+    }),
+    acceptingMembers: z.boolean(), // Whether the group is accepting new members
+    organizerName: z.string(), // The name of the organizer i.e. "John Doe"
+    organizerEmail: z.string().email(), // The email of the organizer
+    organizerImage: z.object({
+      // Organizer's profile image
+      url: z.string().url(),
+      alt: z.string(),
+    }),
+  }),
+})
+
+export const collections = {
+  groups: groupsCollection,
+}
