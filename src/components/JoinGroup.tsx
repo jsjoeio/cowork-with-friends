63a35da2cde92b8eee22e481972058f0efdf4465
Joe Previte <jjprevite@gmail.com>
2024-03-04 04:32:57 +0000

    fix: update styles for individual page

--- src/components/JoinGroup.tsx
+++ src/components/JoinGroup.tsx
@@ -5,12 +5,12 @@ type Props = {
 
 export function JoinGroup({ link, text }: Props) {
   return (
-    <div>
+    <div className="group">
       <div className="inline-block relative z-[2] w-full xl:w-auto xl:w-[300px]">
         <a
           href={link}
           target="_blank"
-          className="border-2 border-light-green relative z-2 w-full text-[18px] z-1 font-bold rounded-full flex justify-center text-center whitespace-nowrap"
+          className="border-2 border-light-green relative z-2 w-full text-[18px] z-1 font-bold rounded-full flex justify-center text-center whitespace-nowrap hover:bg-dark-orange group-hover:border-light-orange"
           style={{ padding: "18px 40px" }}
         >
           <span className="relative flex gap-x-2">
@@ -18,7 +18,7 @@ export function JoinGroup({ link, text }: Props) {
             <span className="text-white">{text}</span>
           </span>
         </a>
-        <span className="block absolute z-[-2] w-full h-full bottom-[-4px] right-[-4px] rounded-full bg-dark-green" />
+        <span className="block absolute z-[-2] w-full h-full bottom-[-4px] right-[-4px] rounded-full bg-dark-green group-hover:bg-dark-orange" />
       </div>
     </div>
   )
